aaa7eb3ee0f5c497fa07e0b8bb11e9cca0e48db0
Shahriyor <shahriyorabdullayev2000@gmail.com>
2022-02-07 12:17:40 +0500

    add scanner and share

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.qrcode
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
+import android.provider.MediaStore.Images
+import android.widget.*
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.io.ByteArrayOutputStream
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvMessage:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,28 +32,73 @@ class MainActivity : AppCompatActivity() {
         val qrCodeImage = findViewById<ImageView>(R.id.iv_qr_code)
         var text = findViewById<EditText>(R.id.et_qr_text)
         var btnGenerate = findViewById<Button>(R.id.btn_qr_generate)
+        val btnScan = findViewById<Button>(R.id.btn_qr_scan)
         var btnShare = findViewById<Button>(R.id.btn_qr_share)
+
+        tvMessage = findViewById(R.id.tv_message)
+
+
 
         btnGenerate.setOnClickListener {
             try {
                 val barcodeEncoder = BarcodeEncoder()
                 val bitmap = barcodeEncoder.encodeBitmap(text.text.toString(), BarcodeFormat.QR_CODE, 300, 300)
                 qrCodeImage.setImageBitmap(bitmap)
+
+                getImageUri(this, bitmap)?.let {
+                    shareImage(it)
+                }
+
             }catch(e: Exception){
 
             }
         }
 
+        btnScan.setOnClickListener {
+            val intentIntegrator = IntentIntegrator(this)
+            intentIntegrator.setPrompt("Scan a barcode or QR Code")
+            intentIntegrator.setOrientationLocked(true)
+            intentIntegrator.initiateScan()
+        }
+
 
 
         btnShare.setOnClickListener {
-            
+
+
         }
 
     }
 
 
-    fun shareMultiImage(uri: Uri) {
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, @Nullable data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        // if the intentResult is null then
+        // toast a message as "cancelled"
+        if (intentResult != null) {
+            if (intentResult.contents == null) {
+                Toast.makeText(baseContext, "Cancelled", Toast.LENGTH_SHORT).show()
+            } else {
+                // if the intentResult is not null we'll set
+                // the content and format of scan message
+                tvMessage.setText(intentResult.contents)
+                //tvMessage.setText(intentResult.formatName)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
+        return Uri.parse(path)
+    }
+
+    fun shareImage(uri: Uri) {
         val intent = Intent()
         intent.action = Intent.ACTION_SEND
         intent.putExtra(Intent.EXTRA_STREAM, uri)
